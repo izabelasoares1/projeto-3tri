e7979cf19bf1a2e29a92e91d602534c0ed0e2271
Hagata Eduarda <hagataeduarda15@outlook.com>
2018-07-27 08:56:00 -0300

    double jump

--- Assets/Scripts/PlayerCtrl.cs
+++ Assets/Scripts/PlayerCtrl.cs
@@ -32,6 +32,10 @@ Hurt - 5
 	public bool isGrounded;
 	public LayerMask whatIsGround;
 
+
+	bool canDoubleJump = false;
+	public float delayforDoubleJump = 0.2f;
+
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D>();
@@ -99,7 +103,20 @@ Hurt - 5
 		isJumping = true;
 		rb.AddForce(new Vector2(0f, jumpSpeed));
 		anim.SetInteger("State", 1);
+
+		Invoke("EnableDoubleJump", delayforDoubleJump);
 		}
+
+		if (canDoubleJump && !isGrounded) {
+		rb.velocity = Vector2.zero;
+		rb.AddForce(new Vector2(0f, jumpSpeed));
+		anim.SetInteger("State", 1);
+		canDoubleJump = false;
+		}
+	}
+
+	void EnableDoubleJump() {
+		canDoubleJump = true;
 	}
 
 void OnCollisionEnter2D(Collision2D other) {
