87813ff8b559733487671f1d27bd25efa10bec12
Hagata Eduarda <hagataeduarda15@outlook.com>
2018-08-03 10:44:41 -0300

    moving platform parte 2

---
+++ Assets/Scripts/FeetCtrl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FeetCtrl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
