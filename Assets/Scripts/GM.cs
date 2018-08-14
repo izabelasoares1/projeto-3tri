37593fdbf64da91b4d1809c6b43fe7c506d904ac
Hagata Eduarda <hagataeduarda15@outlook.com>
2018-08-14 09:04:22 -0300

    contando vidas

--- Assets/Scripts/GM.cs
+++ Assets/Scripts/GM.cs
@@ -67,6 +67,7 @@ public class GM : MonoBehaviour {
 
 void DisplayHudData() {
 	ui.hud.txtCoinCount.text = "x " + data.coinCount;
+	ui.hud.txtLifeCount.text = "x " + data.lifeCount;
 	ui.hud.txtTimer.text = "Timer: " + timeLeft.ToString("F1");
 }
 
@@ -74,10 +75,20 @@ public void IncrementCoinCount() {
 	data.coinCount++;
 }
 
+public void DecrementLives() {
+	data.lifeCount--;
+}
+
 public void KillPlayer() {
 	if (player != null){
 		Destroy(player.gameObject);
+		DecrementLives();
+		if (data.lifeCount > 0) {
 		Invoke("RespawnPlayer", timeToRespawn);
+	}
+	else {
+		GameOver();
+		}
 	}
 }
 
